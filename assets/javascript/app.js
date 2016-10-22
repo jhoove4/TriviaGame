1fa0641a90af26f57d094cdcc6044eaf08d83fc2
Josh Hoover <joshhoover@Joshs-MacBook-Air.local>
2016-10-22 09:02:41 -0700

    retry

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,37 @@
 //Var for questions and stuff
+ $(document).ready (function(){
+
 var numQuestions = 8;
 var numChoices = 4;
 var answers = [];
 var timeRemain = 15;
+var start = startButton;
 
 
 //Functions in the game
 //startGame
 
+function startGame(){
+$('#button').on('click', function(start){
+
+}	
+function changeQuestion(){
+
+}
+function getResults(){
+
+}
+function resetGame(){
+
+}
+}
+ (function(){
+	var questions[{
+		question: "What character would steal children from the parents if they couldn't guess his name?",
+		answer: ["Donald Trump", "Rumple Stiltskin", "The Grim Reaper", "The Grinch"],
+		correctAnswer: 1
+	},{
+		question: "Where did Hansel and Gretle get lost?",
+		answer: []
+	}]
+});
