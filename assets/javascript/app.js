698f3e245f7f642a5a7d63909e9d356f503464f8
Josh Hoover <joshhoover@Joshs-MacBook-Air.local>
2016-10-22 16:58:31 -0700

    newer stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,30 @@
 //Var for questions and stuff
- $(document).ready (function(){
+//  $(document).ready (function(){
 
-var numQuestions = 8;
-var numChoices = 4;
-var answers = [];
-var timeRemain = 15;
-var start = startButton;
+// var numQuestions = 8;
+// var numChoices = 4;
+// var answers = [];
+// var timeRemain = 15;
+// var start = startButton;
 
 
 //Functions in the game
 //startGame
 
-function startGame(){
-$('#button').on('click', function(start){
+// function startGame(){
+// $('#button').on('click', function(start){
 
-}	
-function changeQuestion(){
+// }	
+// function changeQuestion(){
 
-}
-function getResults(){
+// }
+// function getResults(){
 
-}
-function resetGame(){
+// }
+// function resetGame(){
 
-}
-}
+// }
+// }
  (function(){
 	var questions[{
 		question: "What character would steal children from the parents if they couldn't guess his name?",
@@ -32,6 +32,31 @@ function resetGame(){
 		correctAnswer: 1
 	},{
 		question: "Where did Hansel and Gretle get lost?",
-		answer: []
-	}]
-});
+		answer: ["Somewhere in the Mid West", "Top of a mountain", "The woods", "A pay toilet in Scotland"],
+		correctAnswer:2
+	},{
+		question: "What character traded the family cow for a handful of magic beans?",
+		answer: ["Gomer Pile", "Jack Skeleington", "Jack B. Nimble", "Jack"],
+		correctAnswer:3
+	},{
+		question: "What story had a neurotic character who thought the sky was falling?",
+		answer: ["Chicken Little", "Stewart Little", "The Ghost and Mr. Chicken", "The Princess and the Pea"],
+		correctAnswer: 0
+	},{
+		question: "Which story had a wolf that could blow down your house and then would eat you?",
+		answer: ["Little Red Riding Hood", "The Three Little Pigs", "UnderWorld", "Donald Trumps Presidential Campaign"],
+		correctAnswer: 1
+	},{
+		question: "Which Disney movie cover picture had to be replaced because of a phalic symbol?",
+		answer: ["Alladin", "Cinderella", "Beauty and the Beast", "The Little Mermaid"],
+		correctAnswer: 3
+	},{
+		question: "In which story featured a white rabbit running away saying 'I'm late for a very important date!'",
+		answer: ["Snow White", "Beauty and the Beast", "Alice in Wonderland", "Reservoir Dogs"],
+		correctAnswer:2
+	},{
+		question: "In what story did a group of dedicated men serve one woman?",
+		answer: ["Englands Recent History", "Pulp FIction", "Dogma", "Snow White and the 7 Dwarves"],
+		correctAnswer: 3
+	},
+}];
